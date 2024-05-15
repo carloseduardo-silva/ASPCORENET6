db7bf16f1ff41e60a652a333b582d2dd82fbed1b
Carlos Eduardo <102683309+carloseduardo-silva@users.noreply.github.com>
2024-05-15 15:21:17 -0300

    Aula 41 modulo 4

---
+++ LanchesMac/LanchesMac/Controllers/LancheController.cs
@@ -0,0 +1,28 @@
+﻿using LanchesMac.Models;
+using LanchesMac.Repositories.Interfaces;
+using LanchesMac.ViewModels;
+using Microsoft.AspNetCore.Mvc;
+
+namespace LanchesMac.Controllers
+{
+    public class LancheController : Controller
+    {
+        private readonly ILancheRepository _lancheRepository;
+
+        public LancheController(ILancheRepository lancheRepository)
+        {
+            _lancheRepository = lancheRepository;
+        }
+
+        public IActionResult List()
+
+        {
+            // var lanche = _lancheRepository.Lanches;
+            // 
+            var lanchesListViewModel = new LancheListViewModel();
+            lanchesListViewModel.Lanches = _lancheRepository.Lanches;
+            lanchesListViewModel.CategoriaAtual = "Categoria Atual";
+            return View(lanchesListViewModel);
+        }
+    }
+}
